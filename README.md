450c2063d26cdfb58af7f4142eb7c0de6a7195c7
biaa04 <93286481+biaa04@users.noreply.github.com>
2025-07-14 15:27:19 -0300

    alterção final

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-- sudo apt install libjson-c-dev
-- sudo apt install gcc (rodar c)
-- As outras libs já são do Ubunto
-- Comando para executar: gcc -o logger_attacg logger_attach.c -ljson-c
+# Syscall-Tracer
+
+Um rastreador de chamadas de sistema (syscalls) feito em C que utiliza ptrace para interceptar as chamadas feitas por um processo em execução. Os dados são exportados em formato JSON e também exibidos no terminal com argumentos legíveis.
+
+---
+
+## Requisitos
+
+- Linux
+- Compilador C (gcc)
+- Biblioteca [json-c](https://github.com/json-c/json-c)
+
+### Instalação do json-c no Ubuntu/Debian:
+```bash
+sudo apt update
+sudo apt install libjson-c-dev
+```
+
+### Como executar
+```
+    gcc -o logger_attacg logger_attach.c -ljson-c
+    sudo ./logger_attarch <PID>
+```
+
+- O script tem como saida um arquivo json com as informações sobre o processo.
--- logger_attach.c
+++ logger_attach.c
@@ -11,6 +11,7 @@
 #include <time.h>
 #include <json-c/json.h>
 
+// Tabela parcial de syscalls comuns
 const char *syscall_names[450] = {
     [0] = "read",
     [1] = "write",
@@ -18,16 +19,29 @@ const char *syscall_names[450] = {
     [3] = "close",
     [5] = "fstat",     
     [8] = "lseek",     
+    [9] = "mmap",
     [16] = "ioctl",
-    [60] = "exit",
     [39] = "getpid",
-    [62] = "kill",
     [57] = "fork",
     [59] = "execve",
-    [257] = "openat",
-    [9] = "mmap",
-    [231] = "exit_group"
+    [60] = "exit",
+    [62] = "kill",
+    [231] = "exit_group",
+    [257] = "openat"
 };
+
+// Converte código de sinal para nome legível
+const char *get_signal_name(long sig) {
+    switch (sig) {
+        case 1: return "SIGHUP";
+        case 2: return "SIGINT";
+        case 3: return "SIGQUIT";
+        case 9: return "SIGKILL";
+        case 11: return "SIGSEGV";
+        case 15: return "SIGTERM";
+        default: return "UNKNOWN";
+    }
+}
 
 // Gera timestamp legível
 void get_timestamp(char *buffer, size_t size) {
@@ -37,14 +51,14 @@ void get_timestamp(char *buffer, size_t size) {
     strftime(buffer, size, "%Y-%m-%d %H:%M:%S", tm_info);
 }
 
-// Loga a syscall
+// Loga uma syscall
 void log_syscall(pid_t pid, struct user_regs_struct *regs, int entering, FILE *json_file) {
     char timestamp[64];
     get_timestamp(timestamp, sizeof(timestamp));
 
     const char *name = syscall_names[regs->orig_rax] ? syscall_names[regs->orig_rax] : "unknown";
 
-    // JSON
+    // Criação do objeto JSON
     struct json_object *obj = json_object_new_object();
     json_object_object_add(obj, "timestamp", json_object_new_string(timestamp));
     json_object_object_add(obj, "pid", json_object_new_int(pid));
@@ -52,12 +66,20 @@ void log_syscall(pid_t pid, struct user_regs_struct *regs, int entering, FILE *j
     json_object_object_add(obj, "number", json_object_new_int64(regs->orig_rax));
 
     if (entering) {
-        // Terminal
         printf("[%s] %s(", timestamp, name);
-        printf("arg1=%p, arg2=%p, arg3=%p", (void *)regs->rdi, (void *)regs->rsi, (void *)regs->rdx);
+
+        // Exibição legível para syscall kill
+        if (regs->orig_rax == 62) { // kill(pid, sig)
+            printf("arg1=pid:%lld, arg2=sig:%lld (%s), arg3=%lld",
+                   regs->rdi, regs->rsi, get_signal_name(regs->rsi), regs->rdx);
+        } else {
+            printf("arg1=%lld, arg2=%lld, arg3=%lld",
+                   regs->rdi, regs->rsi, regs->rdx);
+        }
+
         printf(")\n");
 
-        // JSON
+        // JSON (modo de entrada)
         json_object_object_add(obj, "tipo", json_object_new_string("enter"));
         struct json_object *args = json_object_new_object();
         json_object_object_add(args, "arg1", json_object_new_int64(regs->rdi));
@@ -65,10 +87,9 @@ void log_syscall(pid_t pid, struct user_regs_struct *regs, int entering, FILE *j
         json_object_object_add(args, "arg3", json_object_new_int64(regs->rdx));
         json_object_object_add(obj, "args", args);
     } else {
-        // Terminal
         printf(" = %lld\n\n", regs->rax);
 
-        // JSON
+        // JSON (modo de saída)
         json_object_object_add(obj, "tipo", json_object_new_string("exit"));
         json_object_object_add(obj, "retorno", json_object_new_int64(regs->rax));
     }
@@ -94,7 +115,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // Anexa ao processo
+    // Anexa ao processo alvo
     if (ptrace(PTRACE_ATTACH, pid, NULL, NULL) == -1) {
         perror("ptrace(ATTACH)");
         fclose(json_file);
@@ -115,6 +136,7 @@ int main(int argc, char *argv[]) {
 
     while (1) {
         if (waitpid(pid, NULL, 0) == -1) break;
+
         if (ptrace(PTRACE_GETREGS, pid, NULL, &regs) == -1) break;
 
         log_syscall(pid, &regs, entering, json_file);
@@ -127,4 +149,4 @@ int main(int argc, char *argv[]) {
     fclose(json_file);
     printf("Desanexado do PID %d e log salvo em syscalls_log.json\n", pid);
     return 0;
-} 
+}
